f51ee79cb0cf672eedbbecd035e57120ea0c0dcb
Akash Kethavath <Akashlucky2331@gmail.com>
2022-01-16 17:33:04 -0600

    final commit

--- public/akash.js
+++ public/akash.js
@@ -1,13 +1,18 @@
 const buttonNumber = document.getElementById("button-number");
 const buttonName = document.getElementById("button-name");
+const rootTag = document.getElementById("root");
 
 buttonNumber.addEventListener('click',fNumber);
 buttonName.addEventListener('click',fName);
 
 function fNumber() {
-    console.log('number button is clicked');
+const n1 = Math.floor(Math.random() * 100);
+const n2 = Math.floor(Math.random() * 100);
+rootTag.innerHTML = 'your lucky numbers are'
+    +n1 + ' and ' + n2;
 }
 
 function fName() {
     console.log('Name button is clicked');
+    rootTag.innerHTML = 'my name is Akash';
 }
